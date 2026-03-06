3d51e477b2dc42644922b55e5e63c0e5a6894242
Maczap99 <90683916+Maczap99@users.noreply.github.com>
2026-03-06 09:37:05 +0100

    edit (SWAC, visualise): add round function to hydrometer

--- swac/components/Visualise/diagrams/Hygrometer.js
+++ swac/components/Visualise/diagrams/Hygrometer.js
@@ -21,30 +21,30 @@ export default class Hygrometer extends Diagram {
             Msg.error('Thermometer', 'There is no datadescription defined.');
             return;
         }
-        
+
         let minValue = this.datadescription.getAttributeMinValue(this.diagramDef.attr);
         let maxValue = this.datadescription.getAttributeMaxValue(this.diagramDef.attr);
-        
+
         // Calculate pointer line
         let line = this.calculatePointer(value, 160, minValue, maxValue);
 
         //creates the svg viewbox
         let svgViewBox = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        svgViewBox.setAttribute("viewBox","0 0 400 400");
-        svgViewBox.setAttribute("width",this.width);
-        svgViewBox.setAttribute("height",this.height);
+        svgViewBox.setAttribute("viewBox", "0 0 400 400");
+        svgViewBox.setAttribute("width", this.width);
+        svgViewBox.setAttribute("height", this.height);
 
         // outer circle
         let svgCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        svgCircle.setAttribute("style","fill: rgb(202, 202, 202);");
-        svgCircle.setAttribute("cx","200");
-        svgCircle.setAttribute("cy","200");
-        svgCircle.setAttribute("r","200");
+        svgCircle.setAttribute("style", "fill: rgb(202, 202, 202);");
+        svgCircle.setAttribute("cx", "200");
+        svgCircle.setAttribute("cy", "200");
+        svgCircle.setAttribute("r", "200");
         svgViewBox.appendChild(svgCircle);
 
         let definitions = this.datadescription.getDefinitions(this.diagramDef.attr);
-        if(!definitions) {
-            Msg.error('Hygrometer','There are not definitions for >' + this.diagramDef.attr + '< in the datadescription.');
+        if (!definitions) {
+            Msg.error('Hygrometer', 'There are not definitions for >' + this.diagramDef.attr + '< in the datadescription.');
             return document.createElement('span');
         }
         for (let curDef of definitions) {
@@ -54,35 +54,35 @@ export default class Hygrometer extends Diagram {
             let coordinate3 = this.calculatePointer(curDef.maxValue, 145, minValue, maxValue);
             let coordinate4 = this.calculatePointer(curDef.minValue, 145, minValue, maxValue);
             svgPath.setAttribute("style", "fill: " + curDef.col);
-            svgPath.setAttribute("d","M " + coordinate1[0] + " " + coordinate1[1] + " A 170 170 0 0 1 " + coordinate2[0] + " " + coordinate2[1] + " L " + coordinate3[0] + " " + coordinate3[1] + " A 145 145 0 0 0 " + coordinate4[0] + " " + coordinate4[1] + " Z");
+            svgPath.setAttribute("d", "M " + coordinate1[0] + " " + coordinate1[1] + " A 170 170 0 0 1 " + coordinate2[0] + " " + coordinate2[1] + " L " + coordinate3[0] + " " + coordinate3[1] + " A 145 145 0 0 0 " + coordinate4[0] + " " + coordinate4[1] + " Z");
             svgViewBox.appendChild(svgPath);
         }
 
         //creates the pointer
         let svgLine = document.createElementNS("http://www.w3.org/2000/svg", "line");
-        svgLine.setAttribute("style","stroke:rgb(0,0,0);stroke-width:2");
-        svgLine.setAttribute("x1","200");
-        svgLine.setAttribute("y1","200");
+        svgLine.setAttribute("style", "stroke:rgb(0,0,0);stroke-width:2");
+        svgLine.setAttribute("x1", "200");
+        svgLine.setAttribute("y1", "200");
         svgLine.setAttribute("x2", line[0]);
         svgLine.setAttribute("y2", line[1]);
 
         //name of the diagram 
-       let svgDiagramName = document.createElementNS("http://www.w3.org/2000/svg", "text");
-        svgDiagramName.setAttribute("fill","black");
-        svgDiagramName.setAttribute("x","200");
-        svgDiagramName.setAttribute("y","290");
+        let svgDiagramName = document.createElementNS("http://www.w3.org/2000/svg", "text");
+        svgDiagramName.setAttribute("fill", "black");
+        svgDiagramName.setAttribute("x", "200");
+        svgDiagramName.setAttribute("y", "290");
         svgDiagramName.setAttribute("text-anchor", "middle");
         svgDiagramName.setAttribute("font-size", "30");
         svgDiagramName.innerHTML = this.name;
 
         //displays the unit
         let svgDiagramUnit = document.createElementNS("http://www.w3.org/2000/svg", "text");
-        svgDiagramUnit.setAttribute("fill","black");
-        svgDiagramUnit.setAttribute("x","200");
-        svgDiagramUnit.setAttribute("y","255");
+        svgDiagramUnit.setAttribute("fill", "black");
+        svgDiagramUnit.setAttribute("x", "200");
+        svgDiagramUnit.setAttribute("y", "255");
         svgDiagramUnit.setAttribute("text-anchor", "middle");
         svgDiagramUnit.setAttribute("font-size", "50");
-        svgDiagramUnit.innerHTML = value + " " + this.unit;
+        svgDiagramUnit.innerHTML = this.roundMax2(value) + " " + this.unit;
 
         //calculates the scala
         let step_count = 10;
@@ -95,38 +95,38 @@ export default class Hygrometer extends Diagram {
             if (next < maxValue) {
                 valueList[counter] = next;
             } else {
-               valueList[counter] = maxValue; 
+                valueList[counter] = maxValue;
             }
         } while (valueList[counter] < maxValue)
-        
+
         // creates the scala
-        for (let i in valueList){
+        for (let i in valueList) {
             let coordinate1 = this.calculatePointer(valueList[i], 165, minValue, maxValue);
             let coordinate2 = this.calculatePointer(valueList[i], 175, minValue, maxValue);
-            
+
             let svgScala = document.createElementNS("http://www.w3.org/2000/svg", "line");
-            svgScala.setAttribute("style","stroke:rgb(0,0,0);stroke-width:1");
+            svgScala.setAttribute("style", "stroke:rgb(0,0,0);stroke-width:1");
             svgScala.setAttribute("x1", coordinate1[0]);
             svgScala.setAttribute("y1", coordinate1[1]);
             svgScala.setAttribute("x2", coordinate2[0]);
             svgScala.setAttribute("y2", coordinate2[1]);
 
             let svgScalaTxt = document.createElementNS("http://www.w3.org/2000/svg", "text");
-            svgScalaTxt.setAttribute("fill","black");
+            svgScalaTxt.setAttribute("fill", "black");
             svgScalaTxt.setAttribute("x", coordinate2[0]);
             svgScalaTxt.setAttribute("y", coordinate2[1]);
-            if (valueList[i] > ((maxValue-minValue)/3)*2+minValue) {
+            if (valueList[i] > ((maxValue - minValue) / 3) * 2 + minValue) {
                 svgScalaTxt.setAttribute("text-anchor", "start");
-            } else if (valueList[i] < (((maxValue-minValue)/3)*2)+minValue && valueList[i] > ((maxValue-minValue)/3)+minValue) {
+            } else if (valueList[i] < (((maxValue - minValue) / 3) * 2) + minValue && valueList[i] > ((maxValue - minValue) / 3) + minValue) {
                 svgScalaTxt.setAttribute("text-anchor", "middle");
             } else {
-                svgScalaTxt.setAttribute("text-anchor", "end");				
+                svgScalaTxt.setAttribute("text-anchor", "end");
             }
             if (valueList[i] >= 1000) {
                 svgScalaTxt.setAttribute("text-anchor", "middle");
             }
             svgScalaTxt.innerHTML = valueList[i];
-            
+
             svgViewBox.appendChild(svgScala);
             svgViewBox.appendChild(svgScalaTxt);
         }
@@ -136,7 +136,7 @@ export default class Hygrometer extends Diagram {
 
         return svgViewBox;
     }
-    
+
     /**
      * Draws the diagram for a set
      * 
@@ -159,14 +159,18 @@ export default class Hygrometer extends Diagram {
     calculatePointer(value, r, absoluteMin, absoluteMax) {
         let degree;
         let coordinates;
-        degree = ((value-absoluteMin)/(absoluteMax-absoluteMin))*180;
-        degree = degree * (Math.PI/180);
+        degree = ((value - absoluteMin) / (absoluteMax - absoluteMin)) * 180;
+        degree = degree * (Math.PI / 180);
         //if (degree > 90) {
-                //degree = 90 - (degree - 90);
-                //coordinates = [200+(Math.cos(degree)*r), 200-(Math.sin(degree)*r)];
+        //degree = 90 - (degree - 90);
+        //coordinates = [200+(Math.cos(degree)*r), 200-(Math.sin(degree)*r)];
         //} else {		
-                coordinates = [200-(Math.cos(degree)*r), 200-(Math.sin(degree)*r)];
+        coordinates = [200 - (Math.cos(degree) * r), 200 - (Math.sin(degree) * r)];
         //}
         return coordinates;
+    }
+
+    roundMax2(num) {
+        return Math.round(num * 100) / 100;
     }
 }
