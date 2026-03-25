bc2d63b8ce19e30cbc716a43f67c7ba18d09589e
FlorianFehring <ffehring@hsbi.de>
2026-03-25 17:25:52 +0100

    Added: Dashboard: English translation

---
+++ swac/components/Dashboard/langs/en.js
@@ -0,0 +1,23 @@
+var Dashboard_en = {
+    visibility_hide: 'Hide',
+    visibility_show: 'show',
+    hiddencards: 'Hidden cards',
+    data_span: 'Showing timeframe',
+    data_from: 'from',
+    data_until: 'till',
+    advanced_options: 'Other options',
+    title: 'Card title',
+    desc: 'Card description',
+    datasource: 'Datasource',
+    data_comp: 'Component to view',
+    admopts: 'Administrator Options',
+    card_avail: 'Card available',
+    card_visible_default: 'Card visable (Standard)',
+    data_from_default: 'Data from (Standard)',
+    data_until_default: 'Data till (Standard)',
+    data_from_min: 'Data from (Minimum)',
+    data_until_max: 'Data till (Maximum)',
+    advoptssaveerr: 'Error occured on save.',
+    data_comp_opts: 'Component options'
+};
+export default Dashboard_en;
