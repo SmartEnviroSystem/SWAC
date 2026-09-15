e912738c10cb9bd1a8574e65fec9897ae1bc322f
FlorianFehring <ffehring@hsbi.de>
2026-09-15 09:39:45 +0200

    Removed: Worldmap2D/ToggleClickInteractionButton Pluign: Should be no plugin but maybe core functionality. But the problem that introduced this plugin was allready solved.

--- swac/components/Worldmap2d/plugins/ToggleClickInteractionButton/langs/de.js
+++
@@ -1,3 +0,0 @@
-var Worldmap2d_ToggleClickInteractionButton_de = {};
-
-export default Worldmap2d_ToggleClickInteractionButton_de;
--- swac/components/Worldmap2d/plugins/ToggleClickInteractionButton/langs/en.js
+++
@@ -1,3 +0,0 @@
-var Worldmap2d_ToggleClickInteractionButton_en = {};
-
-export default Worldmap2d_ToggleClickInteractionButton_en;
